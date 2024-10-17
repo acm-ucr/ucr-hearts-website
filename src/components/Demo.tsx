21e1ec5a486719e5898cae1ad2a672498d0370a6
qhgill <qhgill2@gmail.com>
2024-10-17 12:42:42 -0700

    small demo changes

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -4,6 +4,9 @@ const Demo = () => {
   return (
     <div className="flex flex-col bg-hearts-brown p-10">
       <Button />
+      <p className="">
+        about text
+      </p>
     </div>
   );
 };
