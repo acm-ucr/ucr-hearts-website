a83211a327f2603085e26b9269e07b9a41f9b026
Shing Hung <yhung022@ucr.edu>
2024-10-17 19:50:17 -0700

    save copy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,23 +4,18 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="relative h-screen w-full overflow-hidden bg-hearts-beige">
-      <div className="pointer-events-none absolute inset-0 z-20">
-        <Image
-          src={image1}
-          alt="404 Background"
-          className="h-full w-full object-contain"
-        />
-      </div>
-
-      <div className="absolute inset-0 z-10 flex items-end justify-center">
-        <Link
-          href="/"
-          className="mb-[12%] w-fit rounded-full border-4 border-hearts-brown bg-hearts-light-brown px-16 py-4 font-hearts text-3xl text-white shadow-lg shadow-gray-500 duration-300 hover:opacity-75"
-        >
-          back to home
-        </Link>
-      </div>
+    <div className="relative w-full overflow-hidden bg-hearts-beige py-[7%]">
+      <Image
+        src={image1}
+        alt="404 Background"
+        className="w-full object-cover"
+      />
+      <Link
+        href="/"
+        className="absolute bottom-[20%] left-[41%] h-fit w-fit cursor-pointer rounded-full border-[3px] border-hearts-brown bg-hearts-light-brown px-14 py-3 font-hearts text-2xl font-extralight text-white shadow-lg shadow-gray-500 duration-300 hover:opacity-75"
+      >
+        back to home
+      </Link>
     </div>
   );
 };
--- src/components/home/Join.tsx
+++ src/components/home/Join.tsx
@@ -5,53 +5,51 @@ import Link from "next/link";
 
 const Join = () => {
   return (
-    <div className="">
-      <div className="relative flex w-full flex-col items-center">
-        <Image
-          src={backgroundImage}
-          alt="Background"
-          className="h-auto w-full object-cover"
-        />
+    <div className="relative flex w-full flex-col items-center">
+      <Image
+        src={backgroundImage}
+        alt="Background"
+        className="h-auto w-full object-cover"
+      />
 
-        <div className="absolute flex w-1/3 flex-col items-center">
-          <p className="mx-3 font-hearts text-2xl font-light text-hearts-brown">
-            How to Join
-          </p>
-          <div className="h-[2.5px] w-1/6 rounded-lg bg-hearts-brown"></div>
-          <p className="my-8 items-center whitespace-nowrap text-center font-hearts text-xl font-light text-hearts-blue">
-            Membership is open to everyone interested in the
-            <br />
-            <span>healthcare field!</span>
-          </p>
+      <div className="absolute flex w-1/3 flex-col items-center">
+        <p className="mx-3 font-hearts text-3xl font-light text-hearts-light-brown">
+          How to Join
+        </p>
+        <div className="h-[2.5px] w-1/6 rounded-lg bg-hearts-light-brown"></div>
+        <p className="my-8 items-center whitespace-nowrap text-center font-hearts text-2xl font-extralight text-hearts-blue">
+          Membership is open to everyone interested in the
+          <br />
+          <span>healthcare field!</span>
+        </p>
 
-          <Link
-            href="http://google.com"
-            className="border-3 my-1 w-fit rounded-2xl border-hearts-blue bg-hearts-blue px-9 py-3 font-hearts text-xl text-hearts-beige drop-shadow-xl hover:opacity-75 active:opacity-100"
-            target="_blank"
-          >
-            Membership Form
-          </Link>
+        <Link
+          href="http://google.com"
+          className="border-3 my-1 w-fit cursor-pointer rounded-2xl border-hearts-blue bg-hearts-blue px-16 py-3 font-hearts text-2xl font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-50"
+          target="_blank"
+        >
+          Membership Form
+        </Link>
 
-          <p className="my-8 items-center whitespace-nowrap text-center font-hearts text-xl font-light text-hearts-blue">
-            Committees are only open to members
-          </p>
+        <p className="my-8 items-center whitespace-nowrap text-center font-hearts text-2xl font-extralight text-hearts-blue">
+          Committees are only open to members
+        </p>
 
-          <Link
-            href="http://google.com"
-            className="border-3 my-1 w-fit rounded-2xl border-hearts-blue bg-hearts-blue px-9 py-3 font-hearts text-xl text-hearts-beige drop-shadow-xl hover:opacity-75 active:opacity-100"
-            target="_blank"
-          >
-            Apply for committee here
-          </Link>
+        <Link
+          href="http://google.com"
+          className="border-3 my-1 w-fit cursor-pointer rounded-2xl border-hearts-blue bg-hearts-blue px-9 py-3 font-hearts text-2xl font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-100"
+          target="_blank"
+        >
+          Apply for committee here
+        </Link>
 
-          <p className="my-12 items-center whitespace-nowrap text-center font-hearts text-xl font-light text-hearts-brown">
-            Our general meetings are Mondays at{" "}
-            <span className="text-hearts-brown text-opacity-50">6PM</span> at{" "}
-            <span className="text-hearts-brown text-opacity-50">
-              LOCATION TBD
-            </span>
-          </p>
-        </div>
+        <p className="my-12 items-center whitespace-nowrap text-center font-hearts text-2xl font-extralight text-hearts-brown">
+          Our general meetings are Mondays at{" "}
+          <span className="text-hearts-brown text-opacity-50">6PM</span> at{" "}
+          <span className="text-hearts-brown text-opacity-50">
+            LOCATION TBD
+          </span>
+        </p>
       </div>
     </div>
   );
