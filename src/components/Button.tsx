b56742ec6a4eebb794e637cef0d8a601d6bdc6c5
qhgill <qhgill2@gmail.com>
2024-10-18 20:50:11 -0700

    demo week 2 post-meeting updates

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,12 @@ import logo from "@/public/logo.webp";
 
 const Button = ({ link, text }: { link: string; text: string }) => {
   return (
-    <div className="flex h-auto w-1/12 flex-col items-center py-3 duration-300 hover:cursor-pointer hover:opacity-50">
+    <div className="flex h-auto w-1/12 flex-col items-center py-3 duration-300 hover:cursor-pointer hover:opacity-50 active:opacity-30">
       <Image src={logo} alt="logo" className="aspect-square object-contain" />
       <Link
         href={link}
         target="_blank"
-        className="bg-hearts-light-blue px-3 py-2 font-hearts text-2xl font-bold text-hearts-blue"
+        className="bg-hearts-light-blue px-3 py-2 font-hearts text-[2vw] font-bold text-hearts-blue"
       >
         {text}
       </Link>
--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,13 +1,25 @@
 import Button from "@/components/Button";
+import Logo from "@/public/logo.webp";
+import Image from "next/image";
 
 const Demo = () => {
   return (
-    <div className="flex min-h-80 w-2/3 bg-hearts-brown">
-      <Button link="\" text="text" />
-      <Button link="\" text="text" />
-      <Button link="\" text="text" />
-      <Button link="\" text="text" />
-      <Button link="\" text="text" />
+    <div className="w-page flex flex-col items-center py-10">
+      <div className="relative flex min-h-80 w-[50vw] items-center justify-evenly bg-hearts-brown">
+        <Button link="\" text="text 1" />
+        <Button link="\" text="text 2" />
+        <Button link="\" text="text 3" />
+        <Button link="\" text="text 4" />
+        <Button link="\" text="text 5" />
+        <Button link="\" text="text 6" />
+      </div>
+      <Image
+        src={Logo}
+        alt="img"
+        className="absolute left-10 top-[50%] w-1/12"
+      />
+      <Image src={Logo} alt="img" className="h-20 w-full object-fill" />
+      <Image src={Logo} alt="img" className="h-20 w-full object-contain" />
     </div>
   );
 };
