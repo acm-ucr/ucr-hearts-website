597c2b887b0bdc2161545570f0dd4f0ab2b12e41
minh <minhhdtran@gmail.com>
2024-10-24 14:37:55 -0700

    add board members to board page

--- src/components/boards/BoardMembers.tsx
+++ src/components/boards/BoardMembers.tsx
@@ -1,18 +1,29 @@
 import Board from "./Board";
-import logo from "@/public/logo.webp";
+import { boardInfo } from "@/data/boardInfo";
 
 const BoardMembers = () => {
   return (
-    <div className="">
-      <Board
-        name="name"
-        image={logo}
-        pos="position"
-        desc="description"
-        exc="extracurriculurs"
-        mail="/"
-        insta="/"
-      />
+    <div className="mb-[5vh] grid grid-cols-2 gap-x-[15vw] gap-y-[10vh]">
+      {boardInfo.map((CARD, index) => (
+        <div
+          key={index}
+          className={
+            index === boardInfo.length - 1
+              ? "col-span-2 flex justify-center"
+              : ""
+          }
+        >
+          <Board
+            image={CARD.image}
+            name={CARD.name}
+            pos={CARD.pos}
+            desc={CARD.desc}
+            excs={CARD.excs}
+            mail={CARD.mail}
+            insta={CARD.insta}
+          />
+        </div>
+      ))}
     </div>
   );
 };
