f894135addb948f80965dbe5acdb83a4fc6b2579
Ian Negishi <inegi001@ucr.edu>
2024-10-25 12:04:14 -0700

    inegishi/EventCard

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface EventCardProps {
-  image: string;
+  image: StaticImageData;
   text: string;
   title: string;
 }
+
 const EventCard: React.FC<EventCardProps> = ({ image, text, title }) => {
   return (
-    <div className="border-3 mx-auto flex h-80 w-64 flex-row items-center justify-center rounded-3xl border-white bg-white shadow-2xl">
-      <div className="flex flex-col items-center justify-center">
+    <div className="border-3 mx-auto h-80 w-[50%] max-w-screen-md rounded-3xl border-white bg-white shadow-2xl lg:w-64">
+      <div className="flex h-full flex-col items-center justify-center">
         <Image
           src={image}
           alt="InfoPicture"
-          className="md:h-30 mx-auto h-44 w-10/12 items-center rounded-3xl border-2 border-white lg:w-48"
+          className="h-[40vh] w-[80%] max-w-xs rounded-3xl border-2 border-white"
         />
 
-        <div className="mt-2 text-center font-title text-4xl font-light text-hearts-blue">
+        <div className="mt-2 text-center font-title text-2xl font-light text-hearts-blue md:text-3xl">
           {title}
         </div>
-        <div className="text-center font-hearts text-xs font-extralight text-hearts-brown">
+        <div className="md:text-md text-center font-hearts text-sm font-extralight text-hearts-brown">
           {text}
         </div>
       </div>
