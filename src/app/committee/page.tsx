2316230a3a0522c60a445de24848da273b3cfdb2
Cathy Wu <cathywu@MacAir-M1-Cathy.local>
2024-10-25 23:10:40 -0700

    imlemented ComCard.tsx and ComCards.tsx

--- src/app/committee/page.tsx
+++ src/app/committee/page.tsx
@@ -1,10 +1,12 @@
 import Title from "@/components/Title";
-import ComCard from "@/components/committee/ComCard";
+// import ComCard from "@/components/committee/ComCard";
+import ComCards from "@/components/committee/ComCards";
+
 const Committee = () => {
   return (
     <div className="flex w-screen flex-col items-center">
       <Title title="Committee" />
-      <ComCard />
+      <ComCards />
     </div>
   );
 };
