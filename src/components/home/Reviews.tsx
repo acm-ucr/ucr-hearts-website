d9a676de440e4b22b3d1c560832e62afa1f0392a
qhgill <qhgill2@gmail.com>
2024-11-01 00:07:29 -0700

    width adjustment

--- src/components/home/Reviews.tsx
+++ src/components/home/Reviews.tsx
@@ -3,7 +3,7 @@ import { reviews } from "@/data/reviews";
 
 const Reviews = () => {
   return (
-    <div className="my-[1%] flex justify-evenly">
+    <div className="my-[1%] flex w-[90vw] justify-evenly">
       {reviews.map((item, index) => (
         <Review key={index} quote={item.quote} name={item.name} />
       ))}
