e157415d1dba3c65d70797e0023460eae3d0e490
Cathy Wu <cathywu@MacAir-M1-Cathy.local>
2024-11-02 14:20:55 -0700

    attempt at mapping ComCards

--- src/components/committee/ComCards.tsx
+++ src/components/committee/ComCards.tsx
@@ -1,9 +1,23 @@
-const Title = () => {
+import React from "react";
+import ComCard from "./ComCard";
+import comCardInfo from "@/data/comCardInfo";
+
+const ComCards = () => {
   return (
-    <div className="">
-      <p>Title</p>
+    <div className="flex flex-wrap justify-center">
+      {comCardInfo.map((card, index) => (
+        <ComCard
+          key={index}
+          icon={card.icon}
+          title={card.title}
+          text={card.text}
+          item1={card.item1}
+          item2={card.item2}
+          item3={card.item3}
+        />
+      ))}
     </div>
   );
 };
 
-export default Title;
+export default ComCards;
