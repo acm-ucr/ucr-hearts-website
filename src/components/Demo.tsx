f33e34a97821ad38128056a4b967c041f386ec41
qhgill <qhgill2@gmail.com>
2024-11-08 20:38:33 -0800

    animation demo

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -6,8 +6,10 @@ import demoInfo from "@/data/demoinfo";
 const Demo = () => {
   const [inView, ref] = useView();
   return (
-    <div className="w-page flex flex-col items-center py-10">
-      <div className="relative grid min-h-80 w-[90vw] grid-cols-4 gap-[5%] bg-hearts-brown p-5">
+    <div ref={ref} className="w-page flex flex-col items-center py-10">
+      <div
+        className={`relative grid min-h-80 w-[90vw] grid-cols-4 gap-[5%] bg-hearts-brown p-5${inView && "animate-fade-down animate-duration-[1500ms]"}`}
+      >
         {demoInfo.map((info, index) => (
           <div key={index} className="">
             <Button link={info.link} text={info.text} />
