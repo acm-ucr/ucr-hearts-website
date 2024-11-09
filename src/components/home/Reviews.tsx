be1a0b0fb60d1b0ee5b9713880fbd61c755f5a50
Illeyan Atif <illeyan.atif@gmail.com>
2024-11-09 14:00:17 -0800

    Implemented mobile responsiveness

--- src/components/home/Reviews.tsx
+++ src/components/home/Reviews.tsx
@@ -3,7 +3,7 @@ import { reviews } from "@/data/reviews";
 
 const Reviews = () => {
   return (
-    <div className="mb-[15%] mt-[1%] flex w-[90vw] justify-evenly">
+    <div className="mb-[15%] mt-[1%] flex w-[90vw] flex-col items-center space-y-4 md:flex-row md:justify-evenly">
       {reviews.map((item, index) => (
         <Review key={index} quote={item.quote} name={item.name} />
       ))}
