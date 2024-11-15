adff39f6127a64a414819f9ca89c71ad77147ab4
qhgill <qhgill2@gmail.com>
2024-11-14 16:00:26 -0800

    cleanup/adjustments

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -9,16 +9,16 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ image, text, title }) => {
   return (
-    <div className="mx-auto flex w-[50vw] flex-col items-center rounded-[3vw] bg-white px-[3%] pb-[7%] pt-[0.5%] shadow-2xl sm:rounded-[2.5vw] sm:py-[10%] md:w-[80%] md:py-[5%]">
+    <div className="mx-auto flex w-[70vw] flex-col items-center rounded-[8vw] bg-white px-[3%] py-[10%] pb-[7%] pt-[0.5%] shadow-2xl md:w-[80%] md:rounded-[3vw] md:py-[5%]">
       <Image
         src={image}
         alt="InfoPicture"
-        className="mb-4 aspect-[5/3] object-contain sm:w-[100%]"
+        className="mb-4 mt-4 aspect-[5/3] w-[100%] object-contain"
       />
-      <p className="mb-2 font-title text-[4.5vw] font-extralight text-hearts-blue md:text-[2.7vw]">
+      <p className="mb-2 font-title text-[6vw] font-extralight text-hearts-blue md:text-[2.7vw]">
         {title}
       </p>
-      <p className="w-[90%] text-[0.8vw] font-extralight text-hearts-brown sm:text-[2vw] md:text-[1vw]">
+      <p className="w-[90%] text-[3.5vw] font-extralight text-hearts-brown md:text-[.9vw]">
         {text}
       </p>
     </div>
