e98f254f6dc4a2577b199cf8c1389d9c623b4e45
minh <minhhdtran@gmail.com>
2024-11-21 00:53:43 -0800

    implement carousel for mobile

--- src/components/home/Reviews.tsx
+++ src/components/home/Reviews.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Review from "./Review";
 import { reviews } from "@/data/reviews";
-import { useState, RefObject } from "react";
+import { useState, RefObject, useEffect } from "react";
 import { motion, useAnimate, Variant } from "framer-motion";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 type AnimationEntry = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -37,6 +38,11 @@ const shiftVar = {
     opacity: ["0%", "100%", "0%"],
     transition: { duration: 0.5 },
   },
+  fadeIn: {
+    y: ["10%", "0%"],
+    opacity: ["0%", "100%"],
+    transition: { duration: 0.5 },
+  },
 };
 
 const Reviews = () => {
@@ -49,6 +55,34 @@ const Reviews = () => {
   const [rev3, animate3] = useAnimate();
   const [rev4, animate4] = useAnimate();
   const [rev5, animate5] = useAnimate();
+
+  const [isBelow768, setIsBelow768] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+    const handleResize = (e: MediaQueryListEvent) => {
+      if (e.matches) {
+        if (!isBelow768) {
+          setIsBelow768(true);
+          setCurrInd(0);
+        }
+      } else {
+        if (isBelow768) {
+          setIsBelow768(false);
+          setCurrInd(0);
+        }
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleResize);
+
+    if (mediaQuery.matches) {
+      setIsBelow768(true);
+    }
+
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, [isBelow768]);
 
   const animationMap: AnimationMap = {
     leftShift: [
@@ -123,6 +157,26 @@ const Reviews = () => {
         },
       },
     ],
+    leftShiftSmall: [
+      {
+        animate: animate3,
+        target: rev3,
+        props: {
+          x: ["25%", "0%"],
+          opacity: ["0%", "100%"],
+        },
+      },
+    ],
+    rightShiftSmall: [
+      {
+        animate: animate3,
+        target: rev3,
+        props: {
+          x: ["-25%", "0%"],
+          opacity: ["0%", "100%"],
+        },
+      },
+    ],
   };
 
   const runAnimations = (animationKey: keyof AnimationMap) => {
@@ -135,6 +189,8 @@ const Reviews = () => {
 
   const leftShift = () => runAnimations("leftShift");
   const rightShift = () => runAnimations("rightShift");
+  const leftShiftSmall = () => runAnimations("leftShiftSmall");
+  const rightShiftSmall = () => runAnimations("rightShiftSmall");
 
   const prevRev = () => {
     const isFirstRev = currInd === 0;
@@ -144,7 +200,12 @@ const Reviews = () => {
     setCurrInd(newInd);
     setLeftInd(newLeftInd);
     setRightInd(newRightInd);
-    rightShift();
+
+    if (window.innerWidth < 768) {
+      rightShiftSmall();
+    } else {
+      rightShift();
+    }
   };
 
   const nextRev = () => {
@@ -155,7 +216,12 @@ const Reviews = () => {
     setCurrInd(newInd);
     setLeftInd(newLeftInd);
     setRightInd(newRightInd);
-    leftShift();
+
+    if (window.innerWidth < 768) {
+      leftShiftSmall();
+    } else {
+      leftShift();
+    }
   };
 
   const goToRev = (revIdx: number) => {
@@ -167,17 +233,28 @@ const Reviews = () => {
     setLeftInd(newLeftInd);
     setRightInd(newRightInd);
 
-    if (prevInd > revIdx) rightShift();
-    else if (revIdx > prevInd) leftShift();
+    if (prevInd > revIdx) {
+      if (window.innerWidth < 768) {
+        rightShiftSmall();
+      } else {
+        rightShift();
+      }
+    } else if (revIdx > prevInd) {
+      if (window.innerWidth < 768) {
+        leftShiftSmall();
+      } else {
+        leftShift();
+      }
+    }
   };
 
   return (
-    <div className="relative mb-[15%] mt-[1%] flex aspect-[4/1] w-screen flex-col items-center md:flex-row md:justify-evenly">
+    <div className="relative mb-[15%] mt-[1%] flex aspect-[4/1] w-screen flex-row items-center justify-center md:justify-evenly">
       <motion.div
         ref={rev1}
         variants={shiftVar}
         initial="initial"
-        className="absolute right-[66%] -z-10 aspect-[4/3] w-[25vw]"
+        className="absolute right-[66%] -z-10 hidden aspect-[4/3] w-[25vw] md:block"
       >
         <Review quote={reviews[leftInd]?.quote} name={reviews[leftInd]?.name} />
       </motion.div>
@@ -186,26 +263,50 @@ const Reviews = () => {
         variants={shiftVar}
         initial="initial"
         whileInView="shiftIn"
-        className="aspect-[4/3] w-[25vw] cursor-pointer"
+        className="hidden aspect-[4/3] w-[25vw] cursor-pointer md:block"
         onClick={prevRev}
       >
         <Review quote={reviews[leftInd]?.quote} name={reviews[leftInd]?.name} />
       </motion.div>
+
+      <motion.div
+        className="md:hidden"
+        whileInView={{ opacity: ["0%", "100%"] }}
+        transition={{ duration: 0.5 }}
+      >
+        <FaArrowLeft
+          className="mr-[10vw] scale-[125%] cursor-pointer text-hearts-blue active:scale-[150%]"
+          onClick={prevRev}
+        />
+      </motion.div>
+
       <motion.div
         ref={rev3}
         variants={shiftVar}
         initial="initial"
-        whileInView="rightShift"
-        className="aspect-[4/3] w-[25vw] cursor-pointer"
+        whileInView={window.innerWidth < 768 ? "fadeIn" : "rightShift"}
+        className="aspect-[4/3] w-[50vw] cursor-pointer md:w-[25vw]"
       >
         <Review quote={reviews[currInd]?.quote} name={reviews[currInd]?.name} />
       </motion.div>
+
+      <motion.div
+        className="md:hidden"
+        whileInView={{ opacity: ["0%", "100%"] }}
+        transition={{ duration: 0.5 }}
+      >
+        <FaArrowRight
+          className="ml-[10vw] scale-[125%] cursor-pointer text-hearts-blue active:scale-[150%]"
+          onClick={nextRev}
+        />
+      </motion.div>
+
       <motion.div
         ref={rev4}
         variants={shiftVar}
         initial="initial"
         whileInView="rightShift"
-        className="aspect-[4/3] w-[25vw] cursor-pointer"
+        className="hidden aspect-[4/3] w-[25vw] cursor-pointer md:block"
         onClick={nextRev}
       >
         <Review
@@ -218,7 +319,7 @@ const Reviews = () => {
         variants={shiftVar}
         initial="initial"
         whileInView="shiftOut"
-        className="absolute left-[67%] -z-10 aspect-[4/3] w-[25vw]"
+        className="absolute left-[67%] -z-10 hidden aspect-[4/3] w-[25vw] md:block"
       >
         <Review
           quote={reviews[rightInd]?.quote}
@@ -226,14 +327,14 @@ const Reviews = () => {
         />
       </motion.div>
 
-      <div className="absolute -bottom-[2vw] mx-[10%] flex items-center justify-center space-x-[0.5vw]">
+      <div className="absolute -bottom-[20%] mx-[10%] flex items-center justify-center space-x-[1.5vw] md:-bottom-[2vw] md:space-x-[0.5vw]">
         {reviews.map((_, index) => (
           <motion.div
             key={index}
             whileInView={{ opacity: ["0%", "50%", "100%"] }}
             transition={{ duration: 0.5, delay: 0 + index / 10 }}
             onClick={() => goToRev(index)}
-            className={`aspect-square w-[1vw] cursor-pointer rounded-full hover:scale-[130%] ${
+            className={`aspect-square w-[3vw] cursor-pointer rounded-full hover:scale-[130%] md:w-[1vw] ${
               currInd === index ? "bg-hearts-blue" : "bg-gray-300"
             }`}
           />
