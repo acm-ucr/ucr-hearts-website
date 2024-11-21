3fdc8290a8c27a01cb52dd59aec9c05065ba9da4
minh <minhhdtran@gmail.com>
2024-11-21 00:09:21 -0800

    implement carousel for desktop

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,17 +1,22 @@
 export const reviews = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
+      "1Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
     name: "Illeyan Atif",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
+      "2Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
     name: "Illeyan Atif",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
+      "3Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
+    name: "Illeyan Atif",
+  },
+  {
+    quote:
+      "4Lorem ipsum dolor sit amet consectetur. Aenean a nisl consequat ut. Et euismod nisl dui tempus mattis vulputate quam.",
     name: "Illeyan Atif",
   },
 ];
