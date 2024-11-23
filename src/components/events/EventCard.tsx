d6650f01fe1678f8f2793090710663191ad98734
Shing Hung <yhung022@ucr.edu>
2024-11-22 19:29:56 -0800

    save progress

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -9,16 +9,16 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ image, text, title }) => {
   return (
-    <div className="flex w-1/4 flex-col items-center rounded-[8vw] bg-white px-[3%] pb-[7%] pt-[0.5%] md:w-[80%] md:rounded-[3vw]">
+    <div className="flex w-[21vw] flex-col items-center rounded-[3vw] bg-white px-[3%] pb-[7%] pt-[0.5%] shadow-2xl">
       <Image
         src={image}
         alt="InfoPicture"
-        className="aspect-[5/3] object-contain"
+        className="aspect-[4/3] object-contain"
       />
-      <p className="mb-2 font-title text-[6vw] font-extralight text-hearts-blue md:text-[2.7vw]">
+      <p className="font-title text-[2.7vw] font-extralight text-hearts-blue">
         {title}
       </p>
-      <p className="w-[90%] text-[3.5vw] font-extralight text-hearts-brown md:text-[.9vw]">
+      <p className="w-[90%] text-[0.8vw] font-extralight text-hearts-brown">
         {text}
       </p>
     </div>
