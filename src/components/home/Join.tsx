ed5d191ac05bfac6ea2cd93967539728d9f2a54f
Illeyan Atif <illeyan.atif@gmail.com>
2024-11-25 16:50:53 -0800

    Animation for join.tsx

--- src/components/home/Join.tsx
+++ src/components/home/Join.tsx
@@ -1,23 +1,33 @@
+"use client";
+import useView from "../useView";
 import React from "react";
 import Image from "next/image";
 import backgroundImage from "../../../public/join.webp";
 import Link from "next/link";
 
 const Join = () => {
+  const [inView, ref] = useView();
   return (
-    <div className="relative mb-[20%] mt-[30%] flex w-full flex-col items-center md:mb-0 md:mt-0">
+    <div className="relative flex w-full flex-col items-center" ref={ref}>
       <Image
         src={backgroundImage}
         alt="Background"
-        className="h-auto w-full scale-125 object-cover md:scale-100"
+        className={`h-auto w-full object-cover ${inView && "animate-fade-down"}`}
       />
 
-      <div className="absolute top-[-20%] flex w-1/3 flex-col items-center md:top-0">
-        <p className="font-hearts text-[5vw] font-light text-hearts-light-brown md:text-[2.5vw]">
+      <div className="absolute mt-[-3%] flex w-1/3 flex-col items-center md:mt-[0%]">
+        <p
+          className={`font-hearts text-[5vw] font-light text-hearts-light-brown md:text-[2.5vw] ${inView && "animate-fade-down animate-delay-100"}`}
+        >
           How to Join
         </p>
-        <div className="h-[2.5px] w-1/6 rounded-lg bg-hearts-light-brown"></div>
-        <p className="my-[6%] items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-blue md:my-[5%] md:text-[1.7vw]">
+        <div
+          className="h-[2.5px] w-1/6 rounded-lg bg-hearts-light-brown"
+          ref={ref}
+        ></div>
+        <p
+          className={`my-[6%] items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-blue md:my-[5%] md:text-[1.7vw] ${inView && "animate-fade-down animate-delay-200"}`}
+        >
           Membership is open to everyone interested in the
           <br />
           <span>healthcare field!</span>
@@ -25,25 +35,31 @@ const Join = () => {
 
         <Link
           href="http://google.com"
-          className="border-3 my-[1%] w-fit cursor-pointer whitespace-nowrap rounded-md border-hearts-blue bg-hearts-blue px-[15%] py-[2.5%] text-center font-hearts text-[3.5vw] font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-50 md:rounded-2xl md:text-[1.5vw]"
+          className={`border-3 my-[1%] w-fit cursor-pointer rounded-md border-hearts-blue bg-hearts-blue px-[15%] py-[2.5%] text-center font-hearts text-[3.5vw] font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-50 md:rounded-2xl md:text-[1.5vw] ${inView && "animate-fade-right animate-delay-300"}`}
           target="_blank"
+          style={{ minWidth: "150px" }}
         >
           Membership Form
         </Link>
 
-        <p className="my-[6%] items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-blue md:my-[5%] md:text-[1.7vw]">
+        <p
+          className={`my-[6%] items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-blue md:my-[5%] md:text-[1.7vw] ${inView && "animate-delay-400 animate-fade-down"}`}
+        >
           Committees are only open to members
         </p>
 
         <Link
           href="http://google.com"
-          className="border-3 my-1 w-fit cursor-pointer whitespace-nowrap rounded-md border-hearts-blue bg-hearts-blue px-[10%] py-[2.5%] text-center font-hearts text-[3.5vw] font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-100 md:rounded-2xl md:text-[1.5vw]"
+          className={`border-3 my-1 w-fit cursor-pointer rounded-md border-hearts-blue bg-hearts-blue px-[10%] py-[2.5%] text-center font-hearts text-[3.5vw] font-extralight text-hearts-beige drop-shadow-xl duration-300 hover:opacity-75 active:opacity-100 md:rounded-2xl md:text-[1.5vw] ${inView && "animate-fade-left animate-delay-500"}`}
           target="_blank"
+          style={{ minWidth: "200px" }}
         >
           Apply for committee here
         </Link>
 
-        <p className="my-2 items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-brown md:my-12 md:text-[1.5vw]">
+        <p
+          className={`my-2 items-center whitespace-nowrap text-center font-hearts text-[3.5vw] font-extralight text-hearts-brown md:my-12 md:text-[1.5vw] ${inView && "animate-delay-600 animate-fade-down"}`}
+        >
           Our general meetings are Mondays at{" "}
           <span className="text-hearts-brown text-opacity-50">6PM</span> at{" "}
           <span className="text-hearts-brown text-opacity-50">
