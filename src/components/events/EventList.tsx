e2efa98b3170f5218bf57b7280747b90901e1197
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2024-11-28 18:05:26 -0800

    animated event lists.

--- src/components/events/EventList.tsx
+++ src/components/events/EventList.tsx
@@ -1,13 +1,26 @@
+import { motion } from "framer-motion";
 import Event from "./Event";
 import { eventInfo } from "@/data/eventInfo";
 
 const EventList = () => {
   return (
-    <div className="mb-[25%] md:mb-[10%]">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8 }}
+      className="mb-[25%] md:mb-[10%]"
+    >
       <h2 className="font-title text-[6vw] font-light text-hearts-light-brown md:text-[4vw]">
         Upcoming Events
       </h2>
-      <div className="mb-[2%] ml-[13%] h-[1px] w-[30%] bg-hearts-light-brown md:h-[3px] md:w-[22%]" />
+      <motion.div
+        initial={{ scaleX: 0 }}
+        whileInView={{ scaleX: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="mb-[2%] ml-[13%] h-[1px] w-[30%] origin-left bg-hearts-light-brown md:h-[3px] md:w-[22%]"
+      />
       {eventInfo.map((event, index) => (
         <Event
           key={index}
@@ -16,7 +29,7 @@ const EventList = () => {
           link={event.link}
         />
       ))}
-    </div>
+    </motion.div>
   );
 };
 
